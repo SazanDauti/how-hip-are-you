ab2ce93f23e718d068fac515d0d7275ddfbf5f7c
Vlad Chilom <vchilom6@gmail.com>
2017-09-27 23:44:26 -0400

    added request to get list of playlist ids

--- src/data.js
+++ src/data.js
@@ -4,13 +4,20 @@ let request     = require('request-promise')
 let queryString = require('query-string')
 let Promise     = require('bluebird')
 let uuid        = require('uuid')
+let _           = require('lodash')
 let secret      = require('../config/secret')
 
 let hip = Promise.method((accessToken) => {
+  let userId
+  let playlistIds
   return getUserId(accessToken)
   .then((id) => {
-    console.log(id)
-    return id
+    userId = id
+    return getUserPlaylistIds(userId, accessToken)
+  })
+  .then((ids) => {
+    console.log(ids)
+    return userId
   })
 })
 
@@ -25,6 +32,63 @@ let getUserId = Promise.method((accessToken) => {
  .then(response => {
     return JSON.parse(response).id
  })
+})
+
+let getUserPlaylistIds = Promise.method((userId, accessToken) => {
+  let limitMax = 10
+  let getPlaylistsOptions = {
+    url: 'https://api.spotify.com/v1/users/' + userId + '/playlists?limit=' + limitMax +'&offset=' + 0,
+    auth: {
+      'bearer': accessToken
+    }
+  }
+  let playlistIds = []
+  let hasRemainingPlaylists = false
+  return request(getPlaylistsOptions)
+  .then(response => {
+    response = JSON.parse(response)
+    response.items.forEach((item) => {
+      playlistIds.push(item.id)
+    })
+    if (response.next != null) {
+      hasRemainingPlaylists = true
+      let remainingQueries = []
+      let offset = response.limit
+      while (offset <= response.total) {
+        let url = 'https://api.spotify.com/v1/users/' + userId + '/playlists?limit=' + limitMax + '&offset=' + offset
+        remainingQueries.push(getRemainingUserPlaylistIds(url, accessToken))
+        offset += limitMax
+      }
+      return Promise.all(remainingQueries)
+    } else {
+      return playlistIds
+    }
+  })
+  .then((response) => {
+    if (hasRemainingPlaylists) {
+      response.push(playlistIds)
+      playlistIds = _.flatten(response)
+    }
+    return playlistIds
+  })
+})
+
+let getRemainingUserPlaylistIds = Promise.method((url, accessToken) => {
+  let getPlaylistsOptions = {
+    url: url,
+    auth: {
+      'bearer': accessToken
+    }
+  }
+  return request(getPlaylistsOptions)
+  .then(response => {
+    let playlistIds = []
+    response = JSON.parse(response)
+    response.items.forEach((item) => {
+      playlistIds.push(item.id)
+    })
+    return playlistIds
+  })
 })
 
 module.exports = {
