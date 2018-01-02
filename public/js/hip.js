b3ed6212a07dc350674d23d07074d97363e534e1
Vlad Chilom <vchilom6@gmail.com>
2018-01-01 18:05:35 -0700

    added simple bar graph for song data

--- public/js/hip.js
+++ public/js/hip.js
@@ -15,14 +15,59 @@ let getSongData = () => {
       return xhr
     },
     success: (response) => {
+      setupDataView()
       plotData(response)
     }
   })
 }
 
 let plotData = (songs) => {
+  data = new Array(100).fill(5)
+  for (var i = 0; i < songs.length; i++) {
+    index = songs[i].popularity
+    if (index != 0) {
+      data[index - 1] += 1
+    }
+  }
+
+  let height = $('#data-plot').height()
+  let paddedHeight = height - 40
+  let width = $('#data-plot').width()
+  let paddedWidth = width - 50
+
+  let graph = d3.select('#data-plot').append('svg')
+    .attr('height','100%')
+    .attr('width','100%')
+
+  graph.selectAll('rect')
+    .data(data)
+    .enter().append('rect')
+    .attr('height', (data, index) => {
+      return paddedHeight * (data / 100)
+    })
+    .attr('width', (data, index) => {
+      return paddedWidth / 100
+    })
+    .attr('x', (data, index) => {
+      return paddedWidth / 100 * index + 40
+    })
+    .attr('y', (data, index) => {
+      return paddedHeight * (1 - data / 100)
+    })
+    .attr('stroke', 'black')
+    .attr('stroke-width', '1')
+  graph.append('text')
+    .attr('text-anchor', 'middle')
+    .attr('transform', 'translate('+ (18) +','+(height/2)+')rotate(-90)')
+    .text('song popularity');
+  graph.append('text')
+    .attr('text-anchor', 'middle')
+    .attr('transform', 'translate('+ (width/2) +','+(height-(10))+')')
+    .text('number of songs');
+}
+
+let setupDataView = (songs) => {
   $('#data-title').text(window.location.href.split('/hip/')[1] + '\'s library popularity')
-  $('#data-text').text(JSON.stringify(songs))
 }
 
 let showLoadingDiv = () => {
@@ -30,7 +75,6 @@ let showLoadingDiv = () => {
   var bar = new ProgressBar.Line('#loading-bar', { easing: 'easeInOut', strokeWidth: 0.5, duration: 2500, color: '#000000' })
   bar.animate(1)
   setTimeout(() => {
-    console.log(percent)
     $('#loading-div').hide()
   }, 2500)
 }
